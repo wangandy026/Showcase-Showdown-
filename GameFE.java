d0666b7456fbc0cebf48c751373468aea9ec648c
Andy <wangAndy026@gmail.com>
2024-02-14 11:07:56 -0600

    test

--- GameFE.java
+++ GameFE.java
@@ -3,36 +3,67 @@ import java.util.Random;
 public class GameFE {
     public static void main(String[] args) {
         Random Random1 = new Random(); 
-        String [] names = new String[] {"Anas", "Ali", "Bassem", "Mhammed", "Jassem"}; 
+        String [] names = new String[] {"Anas", "Ali", "Bassem", "Mhammed", "Jassrthtrem"}; 
         int [] prize = new int[] {10,20,30,40,50}; 
 
-
         int picks[] = new int[5];
-        System.out.println(Random1); 
+        int tryout[] = {}; 
 
-        for(int i=0; i<5; i++)
+        int nextIndex =0;  
+        int num = CreateRandom();
+        int counter =0; 
+        picks[0] = num; 
+        boolean generateValues = true;
+        while(counter < 6)
         { 
-            boolean unique = true;
-            int Random2 = Random1.nextInt(5);
-            while(unique)
+            int num2 = CreateRandom(); 
+            //generateValues = false; 
+            //check if this number exists on the array 
+            
+            for(int i=0; i<picks.length; i++)
             {
-                for(int j=0; j<5; j++)
+                if(picks[i] != num2)
                 {
-                    if(picks[j] == Random2)
-                        unique = false; 
-                        
+                    picks[i] = num2; 
+                    counter++; 
+                    break; 
                 }
-                unique = false; 
+                else 
+                {
+                    CreateRandom();
+                    break;  
+                } 
             }
-            picks[i] = Random2;
-            System.out.println("***********************************");
-            System.out.println(picks[i]);
-
         }
-
-    
-
+        //System.out.println(CreateRandom()); 
+        for (int number : picks) {
+            System.out.println(number);
+        }
+        
 
     }
-    
+    public static int CreateRandom()
+    {
+        Random RandomNumber = new Random(); 
+        int RandomNumber1 = RandomNumber.nextInt(5); 
+        return RandomNumber1; 
+    }
+    public static boolean unique(int aNumber)
+    {
+        int [] uniqeArr = new int[5]; 
+        for(int i=0; i<5; i++)
+        {
+            if(uniqeArr[i] == aNumber)
+                return true;
+        }
+        return false; 
+    }
+    // public static void printArray() 
+    // {
+    //     int temp [] = new int [5]; 
+    //     for(int i=0; i<5; i++)
+    //     {
+    //         System.out.println(temp[i]);
+    //     }
+    // }
 }
