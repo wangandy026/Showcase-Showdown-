875e2c50ffb638d4d7642c1ef07aa3d7f0bc47d1
Andy <wangAndy026@gmail.com>
2024-02-14 11:10:50 -0600

    test

--- GameFE.java
+++ GameFE.java
@@ -1,9 +1,13 @@
+/*
+ * Written by Anas Bamaroof 
+ */
+
 import java.util.Scanner;
 import java.util.Random;
 public class GameFE {
     public static void main(String[] args) {
         Random Random1 = new Random(); 
-        String [] names = new String[] {"Anas", "Ali", "Bassem", "Mhammed", "Jassem"}; 
+        String [] names = new String[] {"Anas", "Ali", "Bassem", "Mhammed", "Jassefrerfm"}; 
         int [] prize = new int[] {10,20,30,40,50}; 
 
         int picks[] = new int[5];
